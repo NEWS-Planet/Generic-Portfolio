bc9e5ccf39266d657bc93b2eff7c25cd28ad729f
steve desire <stevedesiredev@gmail.com>
2024-01-04 01:04:41 +0100

    micro service pour generer un portfolio suppression dossier frontend

--- frontend_gp/src/privateRoutes.js
+++
@@ -1,8 +0,0 @@
-export default {
-    app: Object.freeze({
-      path: '/*',
-    }),
-    home: Object.freeze({ path: '/home' }),
-    aboutUs: Object.freeze({ path: '/about us' }),
-    talentPool: Object.freeze({ path: '/Talent Pool' })
-  };
